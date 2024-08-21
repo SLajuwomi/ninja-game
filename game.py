fcf237118d5073b31bfef6298cba3b0fecc95b85
SLajuwomi <stephenlajuwomi@gmail.com>
2024-08-21 17:53:08 -0500

    added cloud image to screen changed background color of screen set colorkey of image to remove balck background

--- game.py
+++ game.py
@@ -2,22 +2,54 @@ import pygame
 
 import sys
 
-pygame.init()
+class Game:
+   def __init__(self):
 
-pygame.display.set_caption('ninja game')
+      pygame.init()
 
-screen = pygame.display.set_mode((640, 480))
+      pygame.display.set_caption('ninja game')
 
-clock = pygame.time.Clock() 
+      self.screen = pygame.display.set_mode((640, 480))
 
-while True:
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-      pygame.quit()
-      sys.exit()
-      
+      self.clock = pygame.time.Clock()
 
-  pygame.display.update()
-  # Forces game to run at 60fps
-  # Effectively just a dynamic sleep, sleeps for however long it needs to to run at 60fps
-  clock.tick(60)
+      self.img = pygame.image.load('data/images/clouds/cloud_1.png')
+      # Sets on specific color in image to be transparent 
+      self.img.set_colorkey((0, 0, 0))
+
+      self.img_pos = [160, 260]
+      self.movement = [False, False]
+
+   def run(self):
+      while True:
+         self.screen.fill((14, 219, 248))
+         # Adds 1, -1 or 0 based on what the expression returns
+         # Booleans are converted to integers
+         self.img_pos[1] += (self.movement[1] - self.movement[0]) *5
+         self.screen.blit(self.img, self.img_pos)
+         
+
+
+         for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+               pygame.quit()
+               sys.exit()
+            if event.type == pygame.KEYDOWN:
+               if event.key == pygame.K_UP:
+                  self.movement[0] = True
+               if event.key == pygame.K_DOWN:
+                  self.movement[1] = True
+            if event.type == pygame.KEYUP:
+               if event.key == pygame.K_UP:
+                  self.movement[0] = False
+               if event.key == pygame.K_DOWN:
+                  self.movement[1] = False
+               
+
+         pygame.display.update()
+         # Forces game to run at 60fps
+         # Effectively just a dynamic sleep, sleeps for however long it needs to to run at 60fps
+         self.clock.tick(60)
+
+
+Game().run()
