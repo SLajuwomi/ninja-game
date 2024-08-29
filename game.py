d19a983e154a8b82f87fa5cd0c22870decd2494d
SLajuwomi <stephenlajuwomi@gmail.com>
2024-08-29 12:57:04 -0500

    added code that determines when player is colliding and in what direction changed velocity for when player is falling

--- game.py
+++ game.py
@@ -45,7 +45,7 @@ class Game:
          self.player.update(self.tilemap, (self.movement[1] - self.movement[0], 0))
          self.player.render(self.display)
 
-         print(self.tilemap.physics_rects_around(self.player.pos))
+         # print(self.tilemap.physics_rects_around(self.player.pos))
          
 
 
@@ -63,6 +63,8 @@ class Game:
                   self.movement[0] = True
                if event.key == pygame.K_RIGHT:
                   self.movement[1] = True
+               if event.key == pygame.K_UP:
+                  self.player.velocity[1] = -3
             if event.type == pygame.KEYUP:
                if event.key == pygame.K_LEFT:
                   self.movement[0] = False
--- scripts/entities.py
+++ scripts/entities.py
@@ -7,11 +7,14 @@ class PhysicsEntity:
         self.pos = list(pos)
         self.size = size
         self.velocity = [0, 0]
+        self.collisions = {"up": False, "down": False, "right": False, "left": False}
 
     def rect(self):
         return pygame.Rect(self.pos[0], self.pos[1], self.size[0], self.size[1])
 
     def update(self, tilemap, movement=(0, 0)):
+        self.collisions = {"up": False, "down": False, "right": False, "left": False}
+
         frame_movement = (movement[0] + self.velocity[0], movement[1] + self.velocity[1])
 
         self.pos[0] += frame_movement[0]
@@ -20,8 +23,10 @@ class PhysicsEntity:
             if entity_rect.colliderect(rect):
                 if frame_movement[0] > 0:
                     entity_rect.right = rect.left
+                    self.collisions["right"] = True
                 if frame_movement[0] < 0:
                     entity_rect.left = rect.right
+                    self.collisions["left"] = True
                 self.pos[0] = entity_rect.x 
 
         self.pos[1] += frame_movement[1]
@@ -30,8 +35,10 @@ class PhysicsEntity:
             if entity_rect.colliderect(rect):
                 if frame_movement[1] > 0:
                     entity_rect.bottom = rect.top
+                    self.collisions["down"] = True
                 if frame_movement[1] < 0:
                     entity_rect.top = rect.bottom
+                    self.collisions["up"] = True
                 self.pos[1] = entity_rect.y 
 
         # modifies downard movement (y-axis)
@@ -39,6 +46,10 @@ class PhysicsEntity:
         # min caps how fast you can fall while falling
         #                      max   min
         self.velocity[1] = min(5, self.velocity[1] + 0.1)
+
+        # When the player goes off the edge, their velocity is slowed down
+        if self.collisions["down"] or self.collisions["up"]:
+            self.velocity[1] = 0
 
     def render(self, surface):
         surface.blit(self.game.assets['player'], self.pos)
