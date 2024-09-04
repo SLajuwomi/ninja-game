44c0448a60c1afa8779b88ff76742ed01c5559c3
SLajuwomi <stephenlajuwomi@gmail.com>
2024-09-04 16:17:01 -0500

    added a camera that follows the player

--- game.py
+++ game.py
@@ -7,75 +7,92 @@ from scripts.entities import PhysicsEntity
 from scripts.tilemap import Tilemap
 
 class Game:
-   def __init__(self):
+     def __init__(self):
 
-      pygame.init()
+          pygame.init()
 
-      pygame.display.set_caption('ninja game')
+          pygame.display.set_caption('ninja game')
 
-      self.screen = pygame.display.set_mode((640, 480))
-      # Surface generates a black image at specifed dimension
-      self.display = pygame.Surface((320, 240))
+          self.screen = pygame.display.set_mode((640, 480))
+          # Surface generates a black image at specifed dimension
+          self.display = pygame.Surface((320, 240))
 
-      self.clock = pygame.time.Clock()
+          self.clock = pygame.time.Clock()
 
-      self.movement = [False, False]
+          self.movement = [False, False]
 
-      self.assets = {
-         'decor': load_images('tiles/decor'),
-         'grass': load_images('tiles/grass'),
-         'large_decor': load_images('tiles/large_decor'),
-         'stone': load_images('tiles/stone'),
-         'player': load_image('entities/player.png')
-      }
+          self.assets = {
+          'decor': load_images('tiles/decor'),
+          'grass': load_images('tiles/grass'),
+          'large_decor': load_images('tiles/large_decor'),
+          'stone': load_images('tiles/stone'),
+          'player': load_image('entities/player.png')
+          }
 
-      print(self.assets)
+          print(self.assets)
 
-      #                                            Pos       Size                             
-      self.player = PhysicsEntity(self, 'player', (50, 50), (8, 15))
+          #                                            Pos       Size                             
+          self.player = PhysicsEntity(self, 'player', (50, 50), (8, 15))
 
-      self.tilemap = Tilemap(self, tile_size=16)
+          self.tilemap = Tilemap(self, tile_size=16)
 
-   def run(self):
-      while True:
-         self.display.fill((14, 219, 248))
+          # Starting position of camera
+          self.scroll = [0, 0]
 
-         self.tilemap.render(self.display)
+     def run(self):
+          while True:
+               self.display.fill((14, 219, 248))
 
-         self.player.update(self.tilemap, (self.movement[1] - self.movement[0], 0))
-         self.player.render(self.display)
+               # having the camera follow the player
+               # camera is set by default to top left of screen so we have to subtract the width of the display from the player position to center the camera
+               self.scroll[0] += (self.player.rect().centerx - self.display.get_width() /2 - self.scroll[0]) / 30 # takes 1/30th from the distance remaing to center; so further the player is the faster the camera moves
+               self.scroll[1] += (self.player.rect().centery - self.display.get_width() /2 - self.scroll[1]) / 30 
+               # fixes the jittering
+               # takes whatever the value of scroll is and converts to integer
+               render_scroll = (int(self.scroll[0]), int(self.scroll[1]))
 
-         # print(self.tilemap.physics_rects_around(self.player.pos))
-         
+
+               # # moves the camera
+               # # have to apply camera to both tilemap and player
+               # self.scroll[0] += 1
 
 
-         """  
-         Only works with arrow keys, because some keyboard layouts don't have WASD or are formatted differently
+               self.tilemap.render(self.display, offset=render_scroll)
 
-         One can support both by adding the K_w ... events as well as K_UP...
-         """
-         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-               pygame.quit()
-               sys.exit()
-            if event.type == pygame.KEYDOWN:
-               if event.key == pygame.K_LEFT:
-                  self.movement[0] = True
-               if event.key == pygame.K_RIGHT:
-                  self.movement[1] = True
-               if event.key == pygame.K_UP:
-                  self.player.velocity[1] = -3
-            if event.type == pygame.KEYUP:
-               if event.key == pygame.K_LEFT:
-                  self.movement[0] = False
-               if event.key == pygame.K_RIGHT:
-                  self.movement[1] = False
-               
-         self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()), (0, 0))
-         pygame.display.update()
-         # Forces game to run at 60fps
-         # Effectively just a dynamic sleep, sleeps for however long it needs to to run at 60fps
-         self.clock.tick(60)
+               self.player.update(self.tilemap, (self.movement[1] - self.movement[0], 0))
+               self.player.render(self.display, offset=render_scroll)
+
+               # print(self.tilemap.physics_rects_around(self.player.pos))
+
+
+
+               """  
+               Only works with arrow keys, because some keyboard layouts don't have WASD or are formatted differently
+
+               One can support both by adding the K_w ... events as well as K_UP...
+               """
+               for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                         pygame.quit()
+                         sys.exit()
+                    if event.type == pygame.KEYDOWN:
+                         if event.key == pygame.K_LEFT:
+                              self.movement[0] = True
+                         if event.key == pygame.K_RIGHT:
+                              self.movement[1] = True
+                         if event.key == pygame.K_UP:
+                              self.player.velocity[1] = -3
+                    if event.type == pygame.KEYUP:
+                         if event.key == pygame.K_LEFT:
+                              self.movement[0] = False
+                         if event.key == pygame.K_RIGHT:
+                              self.movement[1] = False
+                    
+               self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()), (0, 0))
+               pygame.display.update()
+               # Forces game to run at 60fps
+               # Effectively just a dynamic sleep, sleeps for however long it needs to to run at 60fps
+               self.clock.tick(60)
 
 
 Game().run()
--- scripts/tilemap.py
+++ scripts/tilemap.py
@@ -12,7 +12,7 @@ NEIGHBOR_OFFSETS = [(-1,0), (-1, -1), (0, -1), (1, -1), (1, 0), (0, 0), (-1, 1),
 PHYSICS_TILES = {'grass', 'stone'}
 
 class Tilemap:
-    def __init__(self, game, tile_size=16):
+     def __init__(self, game, tile_size=16):
         self.game = game
         self.tile_size = tile_size
         self.tilemap = {}
@@ -29,30 +29,31 @@ class Tilemap:
             self.tilemap[str(3 + i) + ';10'] = {'type': 'grass', 'variant': 1, 'pos': (3 + i, 10)}
             self.tilemap[ '10;' + str(5 + i)] = {'type': 'stone', 'variant': 1, 'pos': (10, 5 + i)}
     
-    def tiles_around(self, pos):
-        tiles = []
-        # // - integer division, removes remainder (ex. - 3/2 = 1 not 1.5)
-        tile_loc = (int(pos[0] // self.tile_size), int(pos[1] // self.tile_size)) # converts pixel position into grid position
-        for offset in NEIGHBOR_OFFSETS:
-            # adds base location from tile loc to offsets to get 9 tiles in off set area
-            check_loc = str(tile_loc[0] + offset[0]) + ';' + str(tile_loc[1] + offset[1])
-            if check_loc in self.tilemap:
-                tiles.append(self.tilemap[check_loc])
-        return tiles
+     def tiles_around(self, pos):
+          tiles = []
+          # // - integer division, removes remainder (ex. - 3/2 = 1 not 1.5)
+          tile_loc = (int(pos[0] // self.tile_size), int(pos[1] // self.tile_size)) # converts pixel position into grid position
+          for offset in NEIGHBOR_OFFSETS:
+               # adds base location from tile loc to offsets to get 9 tiles in off set area
+               check_loc = str(tile_loc[0] + offset[0]) + ';' + str(tile_loc[1] + offset[1])
+               if check_loc in self.tilemap:
+                    tiles.append(self.tilemap[check_loc])
+          return tiles
     
-    def physics_rects_around(self, pos):
-        rects = []
-        for tile in self.tiles_around(pos):
-            if tile['type'] in PHYSICS_TILES:
-                rects.append(pygame.Rect(tile['pos'][0] * self.tile_size, tile['pos'][1] * self.tile_size, self.tile_size, self.tile_size))
-        return rects
+     def physics_rects_around(self, pos):
+          rects = []
+          for tile in self.tiles_around(pos):
+               if tile['type'] in PHYSICS_TILES:
+                    rects.append(pygame.Rect(tile['pos'][0] * self.tile_size, tile['pos'][1] * self.tile_size, self.tile_size, self.tile_size))
+          return rects
 
-    def render(self, surface):
-        for tile in self.offgrid_tiles:
-            surface.blit(self.game.assets[tile['type']][tile['variant']], tile['pos'])
+     def render(self, surface, offset=(0, 0)):
+          for tile in self.offgrid_tiles:
+                                                                                               # applying the camera (offset)
+               surface.blit(self.game.assets[tile['type']][tile['variant']], (tile['pos'][0] - offset[0], tile['pos'][1] - offset[1]))
 
-        for loc in self.tilemap:
-            tile = self.tilemap[loc]
-            surface.blit(self.game.assets[tile['type']][tile['variant']], (tile['pos'][0] * self.tile_size, tile['pos'][1] * self.tile_size))
+          for loc in self.tilemap:
+               tile = self.tilemap[loc]
+               surface.blit(self.game.assets[tile['type']][tile['variant']], (tile['pos'][0] * self.tile_size - offset[0], tile['pos'][1] * self.tile_size - offset[1]))
             
         
